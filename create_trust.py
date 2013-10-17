d41d3aca086f8cfa170d6f5396a69dcad085f306
liming <hzliming2013@corp.netease.com>
2013-10-17 20:56:58 +0800

    Add key-gen for ssh

--- create_trust.py
+++ create_trust.py
@@ -1,7 +1,9 @@
 from fabric.api import *
+from fabric.contrib.files import *
 
 def ssh_keygen():
-    run('ssh-keygen')
+    if not exists('~/.ssh/id_rsa.pub'):
+        run('ssh-keygen')
 
 def get_key():
     local('rm -f ./id_ras.pub')
@@ -10,8 +12,10 @@ def get_key():
 def put_key():
     put('./id_rsa.pub', '/tmp/')
     run('cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys2')
+    run('rm -f /tmp/id_rsa.pub')
 
 def create_relation(from_host, remote_hosts):
+    execute(ssh_keygen, hosts=[from_host])
     execute(get_key, hosts=[from_host])
     execute(put_key, hosts=remote_hosts)
     local('rm -f ./id_ras.pub')
@@ -22,6 +26,9 @@ def auto_ssh(*args):
     """
     if len(args) < 2:
         abort('argument >= 2')
+
+    for host in args:
+      execute(ssh_keygen, hosts=[host])
 
     for (i, from_host) in enumerate(args):
         remote_hosts = list(args[0:i])
